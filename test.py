c688a3d94affa60244799da418d0ab9115640f81
josecarlosgomezt <josecarlos.gomez@upf.edu>
2019-02-25 17:34:50 +0100

    testing unit on test

---
+++ test.py
@@ -0,0 +1,16 @@
+
+import pytest
+
+
+from pathlib import Path
+from structure_resolver import *
+import pubchempy as pbc
+import pandas as pd
+import urllib3
+
+csv = Path("test_data/test_data.csv")
+
+
+def test_structure_retrieval():
+    """test compound retrieval"""
+    assert 1 == 1
